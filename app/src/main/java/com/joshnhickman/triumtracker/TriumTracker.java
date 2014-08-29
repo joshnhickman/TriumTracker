6926fd9f43df13555221e184bf114af68b2b6255
Josh Hickman <joshnhickman@gmail.com>
2014-08-28 20:16:38 -0700

    Minor layout changes, force portrait mode

--- app/src/main/java/com/joshnhickman/triumtracker/TriumTracker.java
+++ app/src/main/java/com/joshnhickman/triumtracker/TriumTracker.java
@@ -37,44 +37,38 @@ public class TriumTracker extends Activity {
         listAdapter = new ListAdapter(this, characters);
         listView.setAdapter(listAdapter);
 
+        // Make list items respond to clicks by popping up an alert for information
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
-
-                alert.setTitle(characters.get(position).getCharacterName());
-
                 final EditText input = new EditText(view.getContext());
                 input.setInputType(InputType.TYPE_CLASS_NUMBER);
-                alert.setView(input);
-
                 LayoutInflater inflater = LayoutInflater.from(view.getContext());
                 final View initiativeView = inflater.inflate(R.layout.initiative_setter, parent, false);
-                alert.setView(initiativeView);
-
-                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        String initiative = ((EditText) initiativeView.findViewById(R.id.new_initiative)).getText().toString();
-                        characters.get(position).setInitiativeScore(Integer.valueOf(initiative));
-                        Collections.sort(characters);
-                        listAdapter.notifyDataSetChanged();
-                    }
-                });
-
-                alert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        // Cancelled
-                    }
-                });
-
-                alert.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        characters.remove(position);
-                        listAdapter.notifyDataSetChanged();
-                    }
-                });
-
-                alert.show();
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle(characters.get(position).getCharacterName())
+                        .setView(input)
+                        .setView(initiativeView)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                String initiative = ((EditText) initiativeView.findViewById(R.id.new_initiative)).getText().toString();
+                                characters.get(position).setInitiativeScore(Integer.valueOf(initiative));
+                                Collections.sort(characters);
+                                listAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Cancelled
+                            }
+                        })
+                        .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                characters.remove(position);
+                                listAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .show();
             }
         });
     }
@@ -93,37 +87,32 @@ public class TriumTracker extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_new) {
-
-            AlertDialog.Builder alert = new AlertDialog.Builder(this);
-
-            alert.setTitle("Add new character");
-
             LayoutInflater inflater = LayoutInflater.from(this);
             final View newCharacterView = inflater.inflate(R.layout.character_creator, null);
-            alert.setView(newCharacterView);
+            new AlertDialog.Builder(this)
+                    .setTitle("Add new character")
+                    .setView(newCharacterView)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String characterName = ((EditText) newCharacterView.findViewById(R.id.character_name_edit)).getText().toString();
+                            String playerName = ((EditText) newCharacterView.findViewById(R.id.player_name_edit)).getText().toString();
+                            boolean ally = ((CheckBox) newCharacterView.findViewById(R.id.ally_check)).isChecked();
+                            if (characterName == null || characterName.isEmpty()) {
+                                Toast.makeText(getApplicationContext(), "Character name must be set", Toast.LENGTH_SHORT).show();
+                            } else {
+                                characters.add(new PlayerCharacter(characterName, playerName, ally));
+                                Collections.sort(characters);
+                                listAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            // Cancelled
+                        }
+                    })
+                    .show();
 
-            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    String characterName = ((EditText) newCharacterView.findViewById(R.id.character_name_edit)).getText().toString();
-                    String playerName = ((EditText) newCharacterView.findViewById(R.id.player_name_edit)).getText().toString();
-                    boolean ally = ((CheckBox) newCharacterView.findViewById(R.id.ally_check)).isChecked();
-                    if (characterName == null || characterName.isEmpty()) {
-                        Toast.makeText(getApplicationContext(), "Character name must be set", Toast.LENGTH_SHORT).show();
-                    } else {
-                        characters.add(new PlayerCharacter(characterName, playerName, ally));
-                        Collections.sort(characters);
-                        listAdapter.notifyDataSetChanged();
-                    }
-                }
-            });
-
-            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    // Cancelled
-                }
-            });
-
-            alert.show();
             return true;
         }
         if (id == R.id.action_discard) {
